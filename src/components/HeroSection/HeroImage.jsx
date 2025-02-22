cc20a7c2240188cb46a3bb2e96e540e0375939e9
VINOD KUMAR <168188878+vinodkumartz@users.noreply.github.com>
2025-02-22 22:33:50 +0530

    HeroImage.jsx

--- src/components/HeroSection/HeroImage.jsx
+++ src/components/HeroSection/HeroImage.jsx
@@ -1,36 +1,11 @@
-// import React from 'react'
-
-// const HeroImage = () => {
-//   return (
-//     <div>HeroImage</div>
-//   )
-// }
-
-// export default HeroImage
-import { PiHexagonThin } from "react-icons/pi";
-
 const HeroImage = () => {
   return (
-    <div className="relative self-end h-full w-full items-center justify-center">
-      <div className=" h-full w-full">
-        <img
-          src="../../public/images/about-me.png"
-          alt="Vinod Kumar"
-          className="w-auto h-auto md:max-w-[570px] sm:max-w-[380px] absolute bottom-[0px] z-10 left-[50%] -translate-x-[50%]"
-        />
-        <div className="w-full h-full absolute  bottom-[-20%] -z-10 flex justify-center items-center rotate-90">
-          <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-orange opacity-70  animate-[spin_20s_linear_infinite] " />
-        </div>
-        <div className="w-full h-full absolute  bottom-[-20%] -z-10 flex justify-center items-center rotate-90">
-          <PiHexagonThin className=" md:h-[90%] sm:h-[120%] blur-lg min-h-[600px] w-auto text-orange opacity-70  animate-[spin_20s_linear_infinite] " />
-        </div>
-        <div className="w-full h-full absolute  bottom-[-20%] -z-10 flex justify-center items-center">
-          <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan opacity-70  animate-[spin_20s_linear_infinite] " />
-        </div>
-        <div className="w-full h-full absolute  bottom-[-20%] -z-10 flex justify-center items-center">
-          <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan opacity-70 blur-lg  animate-[spin_20s_linear_infinite] " />
-        </div>
-      </div>
+    <div className="relative flex items-center justify-center h-full w-full">
+      <img
+        src="../../public/images/about-me.png" // Ensure public folder usage
+        alt="Vinod Kumar"
+        className="w-auto h-auto max-w-[200px] md:max-w-[200px] sm:max-w-[200] absolute bottom-0 z-10 left-1/2 transform -translate-x-1/2"
+      />
     </div>
   );
 };
