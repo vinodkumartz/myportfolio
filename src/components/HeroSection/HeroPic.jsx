f982dcdeed38c09f6b1ffae83abd3540db69e847
VINOD KUMAR <168188878+vinodkumartz@users.noreply.github.com>
2025-02-22 17:08:29 +0530

    HeroPic.jsx

--- src/components/HeroSection/HeroPic.jsx
+++ src/components/HeroSection/HeroPic.jsx
@@ -12,15 +12,7 @@ const HeroPic = () => {
       className="relative flex items-center justify-center"
     >
       {/* Profile Image */}
-      <img
-<<<<<<< HEAD
-        src="../../public/images/about-me.png"
-=======
-        src="/images/HexaPic.png" 
->>>>>>> 6c49e6190e40152519ce75444f1f503cf0f7a008
-        alt="Vinod Kumar"
-        className="max-h-[500px] sm:max-h-[400px] w-auto z-10"
-      />
+      <img src="../../public/images/about-me.png" />
 
       {/* Hexagon Rings Positioned Below Image */}
       <div className="absolute -bottom-10 flex justify-center items-center">
